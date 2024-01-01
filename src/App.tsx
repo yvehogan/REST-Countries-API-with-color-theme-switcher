8a0e2c40100c9cb4748916ec51737881b2c5498d
Evelyn Ita <evelyn.ita@wemabank.com>
2024-01-01 16:42:13 +0100

    initial commit for stater file for my REST API countries project

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-600">
       Simple React Typescript Tailwind Sample
-      crt
+      crtmnfexkj.hl .crgew
     </h1>
   );
 }
